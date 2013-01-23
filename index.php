407888835351fa77b132c2442e1bcfacc5fd4e51
boris <boris@13901e78-2507-634d-b62a-01ba92d34a10>
2013-01-23 07:28:52 +0000

    Настройка фреймворка для нескольких серверов

--- index.php
+++ index.php
@@ -1,11 +1,22 @@
 <?php
 
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/../includes/yii-1.1.13/yii.php';
-$config=dirname(__FILE__).'/protected/config/main.php';
+$yii    = 'framework/yii.php';
+$config = dirname(__FILE__).'/protected/config/main.php';
 
-// remove the following lines when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG',true);
+
+if ($_SERVER['HTTP_HOST'] == 'twt-virt-office.local') {
+	$yii    = 'yii-1.1.13/yii.php';
+	$config = dirname(__FILE__).'/protected/config/borro.php';
+
+	defined('YII_DEBUG') or define('YII_DEBUG',true);
+} else if ($_SERVER['HTTP_HOST'] == 'local.twt-virt.ru') {
+	$yii    = dirname(__FILE__).'/../../yii/framework/yii.php';
+	$config = dirname(__FILE__).'/protected/config/local.php';
+
+	defined('YII_DEBUG') or define('YII_DEBUG',true);
+}
+
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
