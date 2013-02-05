8adbe59312b36b2dc097278c269827ff686bf343
yury <yury@13901e78-2507-634d-b62a-01ba92d34a10>
2013-02-05 13:41:51 +0000

    Вместо 404 при истекшей ссылке - нормальная страница с текстом "ошибочка вышла"

--- protected/modules/files/controllers/PublishedController.php
+++ protected/modules/files/controllers/PublishedController.php
@@ -10,71 +10,77 @@ class PublishedController extends Controller
 
 	public function actionShow($key = NULL) {
 		$link = $this->CheckLink($key);
-
-		if ($link->file->is_dir) {
-			$this->render('show', array('link' => $link));
+		if ($link->edate < date('Y-m-d H:i:s', time())) {
+			$this->render('error', array('message' => 'Время действия временной ссылки истекло.'));
 		} else {
-			if (file_exists($link->file->file))
-				Yii::app()->request->sendFile($link->file->name, file_get_contents($link->file->file));
-			else
-				throw new CHttpException(404, 'Файла нет на месте.');
+			if ($link->file->is_dir) {
+				$this->render('show', array('link' => $link));
+			} else {
+				if (file_exists($link->file->file))
+					Yii::app()->request->sendFile($link->file->name, file_get_contents($link->file->file));
+				else
+					throw new CHttpException(404, 'Файла нет на месте.');
+			}
 		}
 	}
 
 	public function actionDownload($key = NULL, $file_id = NULL) {
 		$link = $this->CheckLink($key);
-
-		if (empty($_POST['Files']) && $file_id) {
-			// Запросили конкретный файл из опубликованной директории.
-			$file = $link->file->children()->findByPk($file_id);
-			if (!$file) throw new CHttpException(404, 'Временная ссылка недействительна.');
-			if (file_exists($file->file)) Yii::app()->request->sendFile($file->name, file_get_contents($file->file));
-			else throw new CHttpException(404, 'Файла нет на месте.');
-		} elseif (!empty($_POST['Files']['id'])) {
-			// Запросили архив файлов из опубликованной директории.
-			$criteria = new CDbCriteria();
-			$criteria->addInCondition('id', $_POST['Files']['id']);
-			$files = $link->file->children()->findAll($criteria);
-			if (!$files) throw new CHttpException(404, 'Временная ссылка недействительна.');
-
-			// Создадим, отправим и сотрем архив с файлами.
-			$archive = new ZipArchive();
-			$dir_name = Yii::app()->getBasePath().DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'tmp'.DIRECTORY_SEPARATOR;
-			if (!file_exists($dir_name)) { mkdir($dir_name, 0777, true); }
-			$arch_name = tempnam($dir_name, 'yfa');
-
-			// Уже использованные имена файлов и счетчик-имя для файлов, имена которых не получается сконвертировать в cp866
-			$files_names = array(); $i = 1;
-			if ($archive->open($arch_name , ZipArchive::CREATE) === true) {
-				foreach ($files as $file) {
-					$f_name = @iconv('utf-8', 'cp866', $file->name);
-					if (!$f_name) {
-						// Заменим имя файла на первое доступное.
-						$from = strrpos($file->name, '.');
-						$extension = substr($file->name, $from);
-						if (!@iconv('utf-8', 'cp866', $extension)) {
-							$extension = '.1';
-						}
-						while(in_array($i.$extension, $files_names)) {
-							$i++;
-						}
-						$files_names[] = $f_name = $i.$extension;
-					} else {
-						$files_names[] = $file->name;
-					}
-					// Наконец то добавим файл в архив
-					if (!$file->is_dir && file_exists($file->file))
-						$archive->addFile(realpath($file->file), iconv('utf-8', 'cp866', $f_name));
-				}
-				$archive->close();
-				Yii::app()->request->sendFile($link->file->name.'.zip', file_get_contents($arch_name), null, false);
-				unlink($arch_name);
-				exit;
-			} else {
-				throw new CHttpException(404, 'При создании архива произошла ошибка. Приносим свои извинения.');
-			}
+		if ($link->edate < date('Y-m-d H:i:s', time())) {
+			$this->render('error', array('message' => 'Время действия временной ссылки истекло.'));
 		} else {
-			throw new CHttpException(404, 'Временная ссылка недействительна.');
+			if (empty($_POST['Files']) && $file_id) {
+				// Запросили конкретный файл из опубликованной директории.
+				$file = $link->file->children()->findByPk($file_id);
+				if (!$file) throw new CHttpException(404, 'Временная ссылка недействительна.');
+				if (file_exists($file->file)) Yii::app()->request->sendFile($file->name, file_get_contents($file->file));
+				else throw new CHttpException(404, 'Файла нет на месте.');
+			} elseif (!empty($_POST['Files']['id'])) {
+				// Запросили архив файлов из опубликованной директории.
+				$criteria = new CDbCriteria();
+				$criteria->addInCondition('id', $_POST['Files']['id']);
+				$files = $link->file->children()->findAll($criteria);
+				if (!$files) throw new CHttpException(404, 'Временная ссылка недействительна.');
+
+				// Создадим, отправим и сотрем архив с файлами.
+				$archive = new ZipArchive();
+				$dir_name = Yii::app()->getBasePath().DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'tmp'.DIRECTORY_SEPARATOR;
+				if (!file_exists($dir_name)) { mkdir($dir_name, 0777, true); }
+				$arch_name = tempnam($dir_name, 'yfa');
+
+				// Уже использованные имена файлов и счетчик-имя для файлов, имена которых не получается сконвертировать в cp866
+				$files_names = array(); $i = 1;
+				if ($archive->open($arch_name , ZipArchive::CREATE) === true) {
+					foreach ($files as $file) {
+						$f_name = @iconv('utf-8', 'cp866', $file->name);
+						if (!$f_name) {
+							// Заменим имя файла на первое доступное.
+							$from = strrpos($file->name, '.');
+							$extension = substr($file->name, $from);
+							if (!@iconv('utf-8', 'cp866', $extension)) {
+								$extension = '.1';
+							}
+							while(in_array($i.$extension, $files_names)) {
+								$i++;
+							}
+							$files_names[] = $f_name = $i.$extension;
+						} else {
+							$files_names[] = $file->name;
+						}
+						// Наконец то добавим файл в архив
+						if (!$file->is_dir && file_exists($file->file))
+							$archive->addFile(realpath($file->file), iconv('utf-8', 'cp866', $f_name));
+					}
+					$archive->close();
+					Yii::app()->request->sendFile($link->file->name.'.zip', file_get_contents($arch_name), null, false);
+					unlink($arch_name);
+					exit;
+				} else {
+					throw new CHttpException(404, 'При создании архива произошла ошибка. Приносим свои извинения.');
+				}
+			} else {
+				throw new CHttpException(404, 'Временная ссылка недействительна.');
+			}
 		}
 	}
 
@@ -88,7 +94,6 @@ class PublishedController extends Controller
 		if (!$key) throw new CHttpException(404, 'Временная ссылка недействительна.');
 		$link = FLinks::model()->with('file')->findByAttributes(array('key' => $key));
 		if (!$link) throw new CHttpException(404, 'Временная ссылка недействительна.');
-		if ($link->edate < date('Y-m-d H:i:s', time())) throw new CHttpException(404, 'Время действия ссылки истекло.');
 		return $link;
 	}
 
---
+++ protected/modules/files/views/published/error.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * User: Forgon
+ * Date: 05.02.13
+ */?>
+<div class="row"><div class="span12">
+	<h2>Ошибка</h2>
+	<div class="error">
+		<?=$message?></div>
+</div></div>
