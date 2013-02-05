6e83d5985e403a5fd5461f839f5fc625aeefa651
yury <yury@13901e78-2507-634d-b62a-01ba92d34a10>
2013-02-05 13:23:13 +0000

    Всякие иероглифы и прочие символы из utf-8, которых нет в cp866

--- protected/modules/files/controllers/PublishedController.php
+++ protected/modules/files/controllers/PublishedController.php
@@ -36,16 +36,35 @@ class PublishedController extends Controller
 			$criteria->addInCondition('id', $_POST['Files']['id']);
 			$files = $link->file->children()->findAll($criteria);
 			if (!$files) throw new CHttpException(404, 'Временная ссылка недействительна.');
+
 			// Создадим, отправим и сотрем архив с файлами.
 			$archive = new ZipArchive();
 			$dir_name = Yii::app()->getBasePath().DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'tmp'.DIRECTORY_SEPARATOR;
 			if (!file_exists($dir_name)) { mkdir($dir_name, 0777, true); }
 			$arch_name = tempnam($dir_name, 'yfa');
+
+			// Уже использованные имена файлов и счетчик-имя для файлов, имена которых не получается сконвертировать в cp866
+			$files_names = array(); $i = 1;
 			if ($archive->open($arch_name , ZipArchive::CREATE) === true) {
 				foreach ($files as $file) {
+					$f_name = @iconv('utf-8', 'cp866', $file->name);
+					if (!$f_name) {
+						// Заменим имя файла на первое доступное.
+						$from = strrpos($file->name, '.');
+						$extension = substr($file->name, $from);
+						if (!@iconv('utf-8', 'cp866', $extension)) {
+							$extension = '.1';
+						}
+						while(in_array($i.$extension, $files_names)) {
+							$i++;
+						}
+						$files_names[] = $f_name = $i.$extension;
+					} else {
+						$files_names[] = $file->name;
+					}
+					// Наконец то добавим файл в архив
 					if (!$file->is_dir && file_exists($file->file))
-						$archive->addFile(realpath($file->file), /*Misc::transliterate(*/iconv('utf-8', 'cp866', $file->name)/*)*/);
-						//$archive->addFromString($file->name, file_get_contents($file->file));
+						$archive->addFile(realpath($file->file), iconv('utf-8', 'cp866', $f_name));
 				}
 				$archive->close();
 				Yii::app()->request->sendFile($link->file->name.'.zip', file_get_contents($arch_name), null, false);
