cc943567f7a1a0c0fc400dc5f0b77562d7f96fbf
yury <yury@13901e78-2507-634d-b62a-01ba92d34a10>
2013-03-15 12:52:58 +0000

    Фильтр пользователей по компаниям.

--- protected/controllers/Admin_usersController.php
+++ protected/controllers/Admin_usersController.php
@@ -19,10 +19,25 @@ class Admin_usersController extends CmsController
 			)
 		);
 	}
-	public function actionIndex()
+	public function actionIndex($company = null)
 	{
-		$dataProvider = new CActiveDataProvider(User::model());
-		$this->render('index', array('dataProvider' => $dataProvider));
+		$criteria = new CDbCriteria();
+		if ($company && $company = Company::model()->findByPk($company)) {
+			$criteria->addCondition('user2company.company_id = :company');
+			$criteria->params[':company'] = $company->id;
+		}
+		$companies = array();
+		/** @var $tmp Company[] */
+		$tmp = Company::model()->findAll();
+		if ($tmp) { foreach($tmp as $t) {
+			$companies[] = array('label' => $t->name, 'value' => $t->id);
+		} }
+		unset($t, $tmp);
+//		$dataProvider = new CActiveDataProvider($model, $params);
+//		$dataProvider->getData();
+
+		$dataProvider = new CArrayDataProvider(User::model()->with('user2company')->findAll($criteria));
+		$this->render('index', array('dataProvider' => $dataProvider, 'companies' => $companies, 'company' => $company));
 	}
 
 	public function actionView()
--- protected/views/admin_users/index.php
+++ protected/views/admin_users/index.php
@@ -4,6 +4,10 @@
  * @var $dataProvider CDataProvider
  */
 
+$jui_asset = CHtml::asset(YII_PATH.'/web/js/source/');
+Yii::app()->clientScript->registerCssFile($jui_asset.'/jui/css/base/jquery-ui.css');
+Yii::app()->clientScript->registerCoreScript('jquery.ui');
+
 $this->breadcrumbs=array(
 	'Пользователи',
 );
@@ -11,8 +15,43 @@ $this->pageTitle = 'Пользователи';
 ?>
 <h1><?=$this->pageTitle?></h1>
 
-<a href="<?=$this->createUrl('create')?>">Добавить пользователя</a>
-
+<?php
+	/** @var $form CActiveForm */
+	$form = $this->beginWidget('CActiveForm', array(
+		'action'                    => $this->createUrl(''), // Иначе гет стакается ?company=2&company=5&company=4
+		'id'                        => 'user-list-form',
+		'method'                    => 'get',
+		'enableAjaxValidation'      => false,
+		'enableClientValidation'    => false,
+	));
+	echo CHtml::label('Компания:', 'user_filter_text_field');
+	echo CHtml::textField('', $company ? $company->name : '', array('id' => 'user_filter_text_field'));
+	Yii::app()->clientScript->registerScript('admin_users_filter', 'js:
+	$(document).ready(function(){
+		$(document.getElementById("user_filter_text_field")).autocomplete({
+			source: '.CJavaScript::encode($companies).',
+			minLength: 2,
+			select: function (event, ui) {
+				$(event.target).val(ui.item.label);
+				$(document.getElementById("company_filter_field")).val(ui.item.value).trigger("change");
+				return false;
+			}
+		});
+		$(document).on("change", "#company_filter_field", function(event){
+			$(this.form).trigger("submit");
+		});
+		$(document).on("change", "#user_filter_text_field", function(event) {
+			if (!$(this).val()) {
+				$(document.getElementById("company_filter_field")).val("").trigger("change");
+			}
+		});
+	});
+');
+	echo CHtml::hiddenField('company', $company ? $company->id : '', array('id' => 'company_filter_field'));
+	$this->endWidget();
+//$this->widget('zii.widgets.jui.CJuiAutocomplete', array('name' => 'asd'));
+?>
+	<a href="<?=$this->createUrl('create')?>">Добавить пользователя</a>
 <?php
 $this->widget('ext.bootstrap.widgets.TbGridView', array(
 	'dataProvider' => $dataProvider,
