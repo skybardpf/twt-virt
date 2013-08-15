59efb9dd1afdc36b67b17a1c4391a6af52b374b8
Евгений <le@artektiv.ru>
2013-08-15 17:18:26 +0400

    Страница-заглушка (часть 2).

---
+++ protected/views/display/error.php
@@ -0,0 +1,14 @@
+<?php
+/* @var $this SiteController */
+/* @var $error array */
+
+$this->pageTitle=Yii::app()->name . ' - Error';
+$this->breadcrumbs=array(
+	'Error',
+);
+?>
+
+	Страницы с таким именем не существует.
+
+<div class="error">
+</div>
---
+++ protected/views/display/notexist.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @var $this SitesController
+ * @var $companies Company[]
+ */
+
+$this->pageTitle=Yii::app()->name;
+?>
+
+Сайта с таким именем не существует.
