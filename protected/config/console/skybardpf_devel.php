a5b33101cb29c116551d6da8f0fdc5ccfe4bfab4
Skibardin Andrey <webprofi1983@gmail.com>
2013-11-01 14:16:18 +0400

    Add console config main

--- protected/config/console/skybardpf_devel.php
+++ protected/config/console/skybardpf_devel.php
@@ -1,36 +1,6 @@
 <?php
 
-// This is the configuration for yiic console application.
-// Any writable CConsoleApplication properties can be configured here.
-return array(
-	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'../../',
-	'name'=>'My Console Application',
-
-	// preloading 'log' component
-	'preload'=>array('log'),
-
-	// application components
-	'components'=>array(
-		/*'db'=>array(
-			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-		),*/
-        'db'=>array(
-            'connectionString' => 'mysql:host=mysql-01.artektiv.local;dbname=twt-virt-office',
-            'emulatePrepare' => true,
-            'username' => 'root',
-            'password' => 'qazwsxedc',
-            'charset' => 'utf8',
-            'enableProfiling' => YII_DEBUG,
-            'enableParamLogging' => YII_DEBUG,
-        ),
-		'log'=>array(
-			'class'=>'CLogRouter',
-			'routes'=>array(
-				array(
-					'class'=>'CFileLogRoute',
-					'levels'=>'error, warning',
-				),
-			),
-		),
-	),
+return CMap::mergeArray(
+    require(dirname(__FILE__).'/main.php'),
+    array()
 );
