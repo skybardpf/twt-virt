ce0d18c25acceb0f21b3eb411f3acda4c4f91894
Skibardin Andrey <webprofi1983@gmail.com>
2013-11-01 14:11:41 +0400

    fix yiic migrate path

--- protected/yiic_demo.php
+++ protected/yiic_demo.php
@@ -2,6 +2,6 @@
 
 // change the following paths if necessary
 $yiic='/usr/share/php/libs/yii/1.1.13/yiic.php';
-$config=dirname(__FILE__).'/config/console_demo.php';
+$config=dirname(__FILE__).'/config/console/demo.php';
 
 require_once($yiic);
--- protected/yiic_production.php
+++ protected/yiic_production.php
@@ -2,6 +2,6 @@
 
 // change the following paths if necessary
 $yiic='yiic.php';
-$config=dirname(__FILE__).'/config/console_production.php';
+$config=dirname(__FILE__).'/config/console/production.php';
 
 require_once($yiic);
