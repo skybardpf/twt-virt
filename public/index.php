f20cb10ced474d90cf7f07a0510a8b289bf7adfc
Skibardin Andrey <webprofi1983@gmail.com>
2013-11-28 18:06:04 +0400

    Production config

--- public/index.php
+++ public/index.php
@@ -1,31 +1,28 @@
 <?php
-$yii    = 'yii.php';
-$config = dirname(__FILE__).'/../protected/config/web/devel.php';
+$yii = 'yii.php';
+$config = dirname(__FILE__) . '/../protected/config/web/devel.php';
 
 if (strpos($_SERVER['HTTP_HOST'], 'ioffice-on.com') !== FALSE) {
-        $yii    = 'yii.php';
-        $config = dirname(__FILE__).'/../protected/config/web/production.php';
-        defined('YII_DEBUG') or define('YII_DEBUG',false);
-}
-elseif ($_SERVER['HTTP_HOST'] == 'twt-virt.twtconsult.ru') {
-    defined('YII_DEBUG') or define('YII_DEBUG',false);
-    defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',0);
-    $config = dirname(__FILE__).'/../protected/config/web/production_twt-virt.php';
-}
-elseif ($_SERVER['HTTP_HOST'] == 'twt-virt.artektiv.ru') {
-    defined('YII_DEBUG') or define('YII_DEBUG',false);
-    defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',0);
-    $config = dirname(__FILE__).'/../protected/config/web/demo_artektiv.php';
+    $yii = 'yii.php';
+    $config = dirname(__FILE__) . '/../protected/config/web/production_twt-virt.php';
+    defined('YII_DEBUG') or define('YII_DEBUG', false);
+} elseif ($_SERVER['HTTP_HOST'] == 'twt-virt.twtconsult.ru') {
+    defined('YII_DEBUG') or define('YII_DEBUG', false);
+    defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 0);
+    $config = dirname(__FILE__) . '/../protected/config/web/production_twt-virt.php';
+} elseif ($_SERVER['HTTP_HOST'] == 'twt-virt.artektiv.ru') {
+    defined('YII_DEBUG') or define('YII_DEBUG', false);
+    defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 0);
+    $config = dirname(__FILE__) . '/../protected/config/web/demo_artektiv.php';
 } elseif ($_SERVER['HTTP_HOST'] == 'twt-virt.skybardpf.devel') {
-    defined('YII_DEBUG') or define('YII_DEBUG',true);
-    $config = dirname(__FILE__).'/../protected/config/web/devel.php';
+    defined('YII_DEBUG') or define('YII_DEBUG', true);
+    $config = dirname(__FILE__) . '/../protected/config/web/devel.php';
 } elseif ($_SERVER['HTTP_HOST'] == 'twt-virt') {
-    defined('YII_DEBUG') or define('YII_DEBUG',true);
-    $config = dirname(__FILE__).'/../protected/config/web/local.php';
+    defined('YII_DEBUG') or define('YII_DEBUG', true);
+    $config = dirname(__FILE__) . '/../protected/config/web/local.php';
 }
 
-
-defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
 
 mb_internal_encoding('utf-8');
 require_once($yii);
